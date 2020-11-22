65f57f0ec05495d31fb5e72d07ddcf1576e7565f
Makoto Ogata <ogatamakoto1976@gmail.com>
2020-11-22 22:04:55 +0900

    studying type inference

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('ここにコードを作成します');
+function add(n1, n2, showResult, phrase) {
+    var result = n1 + n2;
+    if (showResult) {
+        console.log(phrase + result);
+    }
+    else {
+        return result;
+    }
+}
+var number1 = 5;
+var number2 = 2.8;
+var showResult = true;
+var resultPhrase = 'Result: ';
+add(number1, number2, showResult, resultPhrase);
--- app.ts
+++ app.ts
@@ -1,1 +1,14 @@
-console.log('ここにコードを作成します');
+function add(n1: number, n2: number, showResult: boolean, phrase: string){
+  const result = n1 + n2;
+  if(showResult) {
+    console.log(phrase + result);
+  } else {
+    return result;
+  }
+}
+
+const number1 = 5;
+const number2 = 2.8;
+const showResult = true;
+const resultPhrase = 'Result: ';
+add(number1, number2, showResult, resultPhrase);
