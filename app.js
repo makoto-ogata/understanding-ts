003253ee5354d4248b592308794cefbacffb5e50
Makoto Ogata <ogatamakoto1976@gmail.com>
2020-11-25 21:06:03 +0900

    Studying Any Type

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
+// const person: {
+//   name: string,
+//   age: number,
+//   hobbies: string[],
+//   role: [number, string],
+// } = {
+//   name: 'yota',
+//   age: 30,
+//   hobbies: ['Sports', 'Cooking'],
+//   role: [2, 'author'],
+// };
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
 var person = {
     name: 'yota',
     age: 30,
     hobbies: ['Sports', 'Cooking'],
-    role: [2, 'author']
+    role: Role.ADMIN
 };
 var favoriteActivities;
 favoriteActivities = ['Sports'];
@@ -11,4 +28,7 @@ console.log(person.name);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobbie = _a[_i];
     console.log(hobbie);
+}
+if (person.role === Role.ADMIN) {
+    console.log('管理者です');
 }
--- app.ts
+++ app.ts
@@ -1,18 +1,28 @@
-const person: {
-  name: string,
-  age: number,
-  hobbies: string[],
-  role: [number, string],
-} = {
+// const person: {
+//   name: string,
+//   age: number,
+//   hobbies: string[],
+//   role: [number, string],
+// } = {
+//   name: 'yota',
+//   age: 30,
+//   hobbies: ['Sports', 'Cooking'],
+//   role: [2, 'author'],
+// };
+
+enum Role {
+  ADMIN, READ_ONLY, AUTHOR
+}
+
+const person = {
   name: 'yota',
   age: 30,
   hobbies: ['Sports', 'Cooking'],
-  role: [2, 'author'],
+  role: Role.ADMIN,
 };
 
-
-let favoriteActivities: string[];
-favoriteActivities = ['Sports'];
+let favoriteActivities: any[];
+favoriteActivities = ['Sports', 5];
 
 // person.role = [0, 'member'];
 
@@ -20,4 +30,8 @@ console.log(person.name);
 
 for (const hobbie of person.hobbies){
   console.log(hobbie);
+}
+
+if(person.role === Role.ADMIN) {
+  console.log('管理者です');
 }
