4f7ddc47c17e9211fe8435d87c415deed538ad5c
Makoto Ogata <ogatamakoto1976@gmail.com>
2020-11-26 23:12:18 +0900

    studying callback

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ function add(n1, n2) {
 function printResult(num) {
     console.log('Result: ' + num);
 }
+function addAndHandle(n1, n2, callback) {
+    var result = n1 + n2;
+    callback(result);
+}
+addAndHandle(10, 20, function (result) {
+    console.log(result);
+});
 var combineValue;
 combineValue = add;
 console.log(combineValue(8, 8));
--- app.ts
+++ app.ts
@@ -6,6 +6,15 @@ function printResult(num: number){
   console.log('Result: ' + num);
 }
 
+function addAndHandle(n1: number, n2: number, callback: (num: number) => void){
+  const result = n1 + n2;
+  callback(result);
+}
+
+addAndHandle(10, 20, (result, n) => {
+  console.log(result);
+});
+
 let combineValue: (a: number, b: number) => number;
 
 combineValue = add;
