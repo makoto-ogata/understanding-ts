98a86b59bbc4e110ce9dd24ef7ae5ab1ddbefa6a
Makoto Ogata <ogatamakoto1976@gmail.com>
2020-11-26 23:46:46 +0900

    studying never type

--- app.js
+++ app.js
@@ -5,3 +5,7 @@ userInput = 'Max';
 if (typeof userInput === 'string') {
     userName = userInput;
 }
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
+}
+generateError('エラーが発生しました', 500);
