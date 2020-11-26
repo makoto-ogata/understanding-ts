de6196a7c00cf618d06e3d1260c93f570dc83409
Makoto Ogata <ogatamakoto1976@gmail.com>
2020-11-26 22:38:24 +0900

    studying function type

--- app.js
+++ app.js
@@ -1,21 +1,10 @@
-function combine(input1, input2, resultConversion) {
-    var result;
-    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
-        result = +input1 + +input2;
-    }
-    else {
-        result = input1.toString() + input2.toString();
-    }
-    return result;
-    // if(resultConversion === 'as-number'){
-    //   return +result;
-    // } else {
-    //   return result;
-    // }
+function add(n1, n2) {
+    return n1 + n2;
 }
-var combineAges = combine(30, 26, 'as-number');
-console.log(combineAges);
-var combineStringAges = combine('30', '26', 'as-number');
-console.log(combineStringAges);
-var combineNames = combine('Max', 'Anna', 'as-text');
-console.log(combineNames);
+function printResult(num) {
+    console.log('Result: ' + num);
+}
+var combineValue;
+combineValue = add;
+console.log(combineValue(8, 8));
+printResult(add(5, 12));
--- app.ts
+++ app.ts
@@ -6,4 +6,11 @@ function printResult(num: number){
   console.log('Result: ' + num);
 }
 
+let combineValue: (a: number, b: number) => number;
+
+combineValue = add;
+
+
+console.log(combineValue(8, 8));
+
 printResult(add(5, 12));
